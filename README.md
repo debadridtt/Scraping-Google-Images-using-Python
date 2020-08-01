414cafb4646dd8b09d4fe86042c4a34bb627f929
Debadri Dutta <debadridtt@gmail.com>
2020-08-01 15:24:09 +0530

    Script Updated on Aug 2020
    Older version of script stopped working

--- README.md
+++ README.md
@@ -5,10 +5,12 @@ Now  scrape as many images as you want, from google images using Python, Chromdr
 Install as `pip install selenium`
 2) [Python 3+](https://www.python.org/download/releases/3.0/?) - Pyhton 3.6+ verion
 3) Download `chromedriver.exe`
-4) Place your `chromedriver.exe` and `any_image.py` file in the same folder
+4) Place your `chromedriver.exe` and `google_image_scraping_script.py` file in the same folder
+
+Line 89 change the query you want to search: `Manchester City` in my case
+Line 94 `links = fetch_image_urls(query,50,wd)`, 50 denotes no. of images you want to download
    > Open your terminal (`cmd` in Windows), change your directory to your Python file location and execute the following query:
-     `python any_image.py travel+photos 500`
-     travel+photos is equivalent to typing "travel photos" in google images search box, so for spaces put a + and 500 depicts the no. of      images you want to scrape.
-     Now in that same directory, you will see a folder `datasets` inside which you will see a folder with the name of search text that you      put in. In my case the folder name will be `travel+photos`.
+     `python google_image_scraping_script.py`
+     Now in that same directory, you will see a folder `datasets` inside which you will see a folder with the name of search text that you      put in. In my case the folder name will be `Manchester City`.
     
-References: <a href="https://github.com/harshitsidhwa/WhatsApp-bot-selenium">Selenium Installation</a> and <a href="https://github.com/atif93/google_image_downloader">Scraping</a>
+References: https://medium.com/@wwwanandsuresh/web-scraping-images-from-google-9084545808a2
---
+++ google_image_scraping_script.py
@@ -0,0 +1,98 @@
+import selenium
+from selenium import webdriver
+import time
+import requests
+import os
+from PIL import Image
+import io
+import hashlib
+
+# All in same directory
+DRIVER_PATH = 'chromedriver.exe'
+
+
+def fetch_image_urls(query:str, max_links_to_fetch:int, wd:webdriver, sleep_between_interactions:int=1):
+    def scroll_to_end(wd):
+        wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(sleep_between_interactions)        
+    
+    # build the google query
+    search_url = "https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&q={q}&oq={q}&gs_l=img"
+
+    # load the page
+    wd.get(search_url.format(q=query))
+
+    image_urls = set()
+    image_count = 0
+    results_start = 0
+    while image_count < max_links_to_fetch:
+        scroll_to_end(wd)
+
+        # get all image thumbnail results
+        thumbnail_results = wd.find_elements_by_css_selector("img.Q4LuWd")
+        number_results = len(thumbnail_results)
+        
+        print(f"Found: {number_results} search results. Extracting links from {results_start}:{number_results}")
+        
+        for img in thumbnail_results[results_start:number_results]:
+            # try to click every thumbnail such that we can get the real image behind it
+            try:
+                img.click()
+                time.sleep(sleep_between_interactions)
+            except Exception:
+                continue
+
+            # extract image urls    
+            actual_images = wd.find_elements_by_css_selector('img.n3VNCb')
+            for actual_image in actual_images:
+                if actual_image.get_attribute('src') and 'http' in actual_image.get_attribute('src'):
+                    image_urls.add(actual_image.get_attribute('src'))
+
+            image_count = len(image_urls)
+
+            if len(image_urls) >= max_links_to_fetch:
+                print(f"Found: {len(image_urls)} image links, done!")
+                break
+            else:
+            	load_more_button = wd.find_element_by_css_selector(".mye4qd")
+            	if load_more_button:
+            		wd.execute_script("document.querySelector('.mye4qd').click();")
+    
+        results_start = len(thumbnail_results)
+
+    return image_urls
+
+def persist_image(folder_path:str,file_name:str,url:str):
+    try:
+        image_content = requests.get(url).content
+
+    except Exception as e:
+        print(f"ERROR - Could not download {url} - {e}")
+
+    try:
+        image_file = io.BytesIO(image_content)
+        image = Image.open(image_file).convert('RGB')
+        folder_path = os.path.join(folder_path,file_name)
+        if os.path.exists(folder_path):
+            file_path = os.path.join(folder_path,hashlib.sha1(image_content).hexdigest()[:10] + '.jpg')
+        else:
+            os.mkdir(folder_path)
+            file_path = os.path.join(folder_path,hashlib.sha1(image_content).hexdigest()[:10] + '.jpg')
+        with open(file_path, 'wb') as f:
+            image.save(f, "JPEG", quality=85)
+        print(f"SUCCESS - saved {url} - as {file_path}")
+    except Exception as e:
+        print(f"ERROR - Could not save {url} - {e}")
+
+if __name__ == '__main__':
+    wd = webdriver.Chrome(executable_path=DRIVER_PATH)
+    queries = ["manchester city"]  #change your set of queries here
+    for query in queries:
+        wd.get('https://google.com')
+        search_box = wd.find_element_by_css_selector('input.gLFyf')
+        search_box.send_keys(query)
+        links = fetch_image_urls(query,50,wd) # 50 denotes no. of images you want to download
+        images_path = 'dataset/'
+        for i in links:
+            persist_image(images_path,query,i)
+    wd.quit()
